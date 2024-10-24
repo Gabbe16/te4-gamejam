3f2006d8a8554fae22487ed4b80c15839d262a50
gabbe16 <gabrielnilsson.hogberg@elev.ga.ntig.se>
2024-10-24 12:32:07 +0200

    Audio update

--- src/Audio.js
+++ src/Audio.js
@@ -4,17 +4,11 @@ export default class BackgroundMusic {
     constructor(game) {
         this.game = game
         this.backgroundMusic = new Audio(backgroundMusicURL)
+        // Loop the background music when it ends
         this.backgroundMusic.loop = true
     }
 
     playBackgroundMusic(){
-        // If the game is over, pause the background music (this does not work yet)
-        if (this.game.gameOver) {
-            this.backgroundMusic.pause()
-            this.backgroundMusic.currentTime = 0
-        } else {
-            // Remember to loop the music
             this.backgroundMusic.play()
-        }
     }
 }
