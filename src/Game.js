f103c5cae6b086b938f3c86a6682e77f4f1cc6b0
Mille <milletedebring.thysell@elev.ga.ntig.se>
2024-10-25 09:55:59 +0200

    fixed problem

--- src/Game.js
+++ src/Game.js
@@ -82,22 +82,33 @@ export default class Game {
       }
       this.player.update(deltaTime)
   
-      
-            // Check collision between player, enemies drops (candy)
+      // Check collision between player, enemies drops (candy)
       this.enemies.forEach((enemy) => {
         enemy.update(this.player, deltaTime)
-        if (this.checkCollision(this.player, enemy)) {  
-          this.audio.playerDamage.volume = 1
+        if (this.checkCollision(this.player, enemy)) {
           this.player.lives--
           enemy.markedForDeletion = true
           if (enemy.type === 'drops') {
             this.player.ammo += 5
             this.player.lives += 1
-            this.audio.playerDamage.volume = 0
           }
-          this.audio.playPlayerDamage()
-          
         }
+  
+        // Check collision between player projectiles and enemies
+        this.player.projectiles.forEach((projectile) => {
+          if (this.checkProjectileCollision(projectile, enemy)) {
+            if (enemy.lives > 1) {
+              enemy.lives -= projectile.damage
+            } else if (enemy.type === 'skeleton') {
+             this.enemies.push(new Candy(this, enemy.x, enemy.y))
+             enemy.markedForDeletion = true
+             this.score += enemy.scoreAmount
+            }
+            if (enemy.type === 'skeleton') {
+              projectile.markedForDeletion = true
+            }
+            
+          }
         })
       })
       this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion)
