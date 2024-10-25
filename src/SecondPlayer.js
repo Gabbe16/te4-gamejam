1724d33ff1f29c1422c7d351b05d10c7c975f206
Mille <milletedebring.thysell@elev.ga.ntig.se>
2024-10-25 13:18:35 +0200

    second player

---
+++ src/SecondPlayer.js
@@ -0,0 +1,176 @@
+import necromancer from './assets/sprites/necromancerSheet.png'
+import Projectile from './Projectile.js'
+
+export default class Player {
+  constructor(game) {
+    this.game = game
+    this.width = 80
+    this.height = 110
+    this.imageheight = 160 
+    this.imagewidth = 128
+    this.x = 740
+    this.y = 393
+
+    this.projectiles = []
+
+    this.speedX = 0
+    this.speedY = 0
+    this.maxSpeed = 3.5
+
+    this.maxAmmo = 20
+    this.ammo = 20
+    this.ammoTimer = 0
+    this.ammoInterval = 500
+
+    this.lives = 10
+
+    this.shooting = false
+
+    // Player spritesheet image
+    const image = new Image()
+    image.src = necromancer
+    this.image = image
+
+    // All animations
+    this.idleAnimation = {
+      frameY: 0,
+      maxFrame: 8
+    }
+
+    this.walkAnimation = {
+      frameY: 1,
+      maxFrame: 8
+    }
+    
+    this.shootingAnimation = {
+      frameY: 2,
+      maxFrame: 13
+    }
+
+    // Sprite animation variables
+    this.frameX = 0
+    this.frameY = 0
+    this.maxFrame = 0
+    this.fps = 20
+    this.timer = 0
+    this.interval = 1000 / this.fps
+
+    // Flip sprite
+    this.flip = false
+  }
+
+  update(deltaTime) {
+  
+
+    // Right and left movement
+    if (this.game.keys.includes('a')) {
+      this.speedX = -this.maxSpeed
+    } else if (
+      this.game.keys.includes('d')
+    ) {
+      this.speedX = this.maxSpeed
+    } else {
+      this.speedX = 0
+    }
+
+    // Up and down movement
+    if (this.game.keys.includes('w')) {
+      this.speedY = -this.maxSpeed
+    } else if (
+      this.game.keys.includes('s')
+    ) {
+      this.speedY = this.maxSpeed
+    } else {
+      this.speedY = 0
+    }
+
+    this.y += this.speedY
+    this.x += this.speedX
+
+    // Sprite animation
+    if (this.speedX < 0) {
+      this.flip = true
+    } else if (this.speedX > 0) {
+      this.flip = false
+    }
+
+    if (this.timer > this.interval) {
+      this.frameX++
+      this.timer = 0
+    } else {
+      this.timer += deltaTime
+    }
+
+    if (this.frameX >= this.maxFrame) {
+      this.frameX = 0
+    }
+
+    // Idle, walk, and shoot animations
+    if (this.shooting) {
+      this.maxFrame = this.shootingAnimation.maxFrame
+      this.frameY = this.shootingAnimation.frameY
+      if (this.frameX === this.shootingAnimation.maxFrame - 1) {
+        this.shooting = false
+      }
+    } else if (this.speedX !== 0 || this.speedY !== 0) {
+      this.maxFrame = this.walkAnimation.maxFrame
+      this.frameY = this.walkAnimation.frameY
+    } else {
+      this.maxFrame = this.idleAnimation.maxFrame
+      this.frameY = this.idleAnimation.frameY
+    }
+
+   
+  }
+
+  draw(context) {
+    if (this.flip) {
+      context.save()
+      context.scale(-1, 1)
+    }
+
+    context.drawImage(
+      this.image,
+      this.frameX * this.width,
+      this.frameY * this.height,
+      this.width,
+      this.height,
+      this.flip ? this.x * -1 - (this.width + 22) : this.x - 22,
+      this.y - 33,
+      this.width * 2,
+      this.height * 2
+    )
+
+    if (this.flip) {
+      context.restore()
+    }
+
+    // Player debugging
+    if (this.game.debug) {
+      context.strokeStyle = '#000'
+      context.strokeRect(this.x, this.y, this.width, this.height)
+      context.lineWidth = 1
+      context.beginPath()
+      const dx = this.game.input.mouseX - (this.x + this.width / 2)
+      const dy = this.game.input.mouseY - (this.y + this.height / 2)
+      const maxLength = 60
+      const angle = Math.atan2(dy, dx)
+      const x = this.x + this.width / 2 + maxLength * Math.cos(angle)
+      const y = this.y + this.height / 2 + maxLength * Math.sin(angle)
+      context.moveTo(this.x + this.width / 2, this.y + this.height / 2)
+      context.lineTo(x, y)
+      context.stroke()
+
+      context.fillStyle = 'black'
+      context.font = '20px Arial'
+      context.fillText(this.lives, this.x, this.y - 5)
+      context.font = '12px Arial'
+      context.fillText(`x: ${this.x.toFixed()}`, this.x + 35, this.y - 5)
+      context.fillText(`y: ${this.y.toFixed()}`, this.x + 35, this.y - 20)
+    }
+
+    
+  }
+
+ 
+}
